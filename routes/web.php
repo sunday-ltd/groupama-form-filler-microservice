361343ffc1fb3877b302c1e13eb3eeee0170e4cc
Balint Molnar <molbal@outlook.com>
2019-11-11 17:30:51 +0100

    Adds phpinfo to test something.

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 	use Fpdf\Fpdf;
 	use Laravel\Lumen\Http\Request;
 
+	$router->get('/phpinfo', function() {
+		phpinfo();
+	});
 	$router->get('render/zalogkotelezettinyilatkozat', ['as' => 'render_zalog_get', 'uses' => 'ZalogRenderController@render']);
 	$router->post('render/zalogkotelezettinyilatkozat', ['as' => 'render_zalog_post', 'uses' => 'ZalogRenderController@render']);
