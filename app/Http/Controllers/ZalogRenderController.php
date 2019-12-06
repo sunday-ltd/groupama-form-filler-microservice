837b47881c85b204fb483e040d33c274d34e61a9
Balint Molnar <molbal@outlook.com>
2019-12-06 10:55:00 +0100

    Removed old comments

--- app/Http/Controllers/ZalogRenderController.php
+++ app/Http/Controllers/ZalogRenderController.php
@@ -84,76 +84,6 @@
 		{
 
 			$data = json_decode($request->input('data'), true);
-			/*
-			$data = [
-				'ados' => [
-					'ingatlan' => [
-						'iranyitoszam' => "1111",
-						'telepules' => "ŰŐ űő",
-						'utcanev' => "Zöldkő utca",
-						'utcaszam' => "9",
-						'emelet' => "",
-						'ajto' => "A",
-						'helyrajzi_szam' => "123/456/789",
-						'alapterulet' => "180",
-						'jelzalog_jogosult' => "Gerő Gábor",
-						'kolcsonszerzodes_szama' => "85489731963",
-						'kolcsonszerzodes_tartalma' => "4189498465",
-						'kolcson_osszege' => "69420420"
-					],
-					'szerzodes' => [
-						'biztositas_neve_1' => "826",
-						'biztositas_neve_2' => "Társasház bizt",
-						'szerzodes_szama' => "BŐŰ-912247",
-						'biztositasi_osszeg' => "70250000",
-					]
-				],
-				'biztositott' => [
-					'nev' => "Molnár Bálint",
-					'iranyitoszam' => "1144",
-					'telepules' => "Budapest",
-					'utcanev' => "Ond Vezér Park",
-					'utcaszam' => "4",
-					'emelet' => "2",
-					'ajto' => "65",
-					'szuletes' => [
-						"hely" => "Budapest",
-						"ev" => "1994",
-						"ho" => "03",
-						"nap" => "03",
-					],
-					'anyja_neve' => "Posteiner Nóra",
-				],
-				'szerzodo' => [
-					'nev' => "Molnár Bálint",
-					'iranyitoszam' => "1144",
-					'telepules' => "Budapest",
-					'utcanev' => "Ond Vezér Park",
-					'utcaszam' => "4",
-					'emelet' => "2",
-					'ajto' => "65",
-					'szuletes' => [
-						"hely" => "Budapest",
-						"ev" => "1994",
-						"ho" => "03",
-						"nap" => "03",
-					],
-					'anyja_neve' => "Posteiner Nóra",
-				],
-				'bank' => [
-					'folyosito' => "Budapest Bank",
-					'meghatalmazott' => "Budapest Bank",
-					'adatkezelo' => "Budapest Bank",
-					'szerzodesszam' => "15034895"
-				],
-				'kelt' => [
-					'ev' => "2019",
-					'ho' => '11',
-					'nap' => '11'
-				]
-			];
-			 */
-
 
 			try {
 
@@ -355,12 +285,6 @@
 		{
 
 			$this->pdf->SetFont('fillfont', '', 14);
-
-//        $txt= str_replace(["ő", "ű", "Ő", "Ű"], ["ö", "ü", "Ö", "Ü"], $text);
-
-
-//        $txt = mb_convert_encoding($txt, "cp1252", "UTF-8");
-
             if ($this->setUppercase) {
                 if (function_exists('mb_strtoupper')) {
                     $txt = mb_strtoupper($txt);
